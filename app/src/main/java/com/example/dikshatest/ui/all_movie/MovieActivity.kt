57ff65bde5e18ec1bd9e81c4122b5085617d32a8
Jhoan River <jhoan.river@inmotion.co.id>
2024-01-22 14:41:50 +0700

    init

---
+++ app/src/main/java/com/example/dikshatest/ui/all_movie/MovieActivity.kt
@@ -0,0 +1,15 @@
+package com.example.dikshatest.ui.all_movie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dikshatest.R
+
+class MovieActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie)
+    }
+}
